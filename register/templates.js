ae94c42b2f625372c41a67119df932840d267e1d
nainnolsan <nain.nolasco.san@gmail.com>
2025-11-04 16:45:05 -0700

    Done again

---
+++ register/templates.js
@@ -0,0 +1,49 @@
+export function participantTemplate(count) {
+  return `
+    <section class="participant${count}">
+      <p>Participant ${count}</p>
+      <div class="item">
+        <label for="fname${count}"> First Name<span>*</span></label>
+        <input id="fname${count}" type="text" name="fname${count}" required />
+      </div>
+      <div class="item activities">
+        <label for="activity${count}">Activity #<span>*</span></label>
+        <input id="activity${count}" type="text" name="activity${count}" />
+      </div>
+      <div class="item">
+        <label for="fee${count}">Fee ($)<span>*</span></label>
+        <input id="fee${count}" type="number" name="fee${count}" value="0" />
+      </div>
+      <div class="item">
+        <label for="date${count}">Desired Date <span>*</span></label>
+        <input id="date${count}" type="date" name="date${count}" />
+      </div>
+      <div class="item">
+        <p>Grade</p>
+        <select id="grade${count}">
+          <option value="" disabled selected></option>
+          <option value="1">1st</option>
+          <option value="2">2nd</option>
+          <option value="3">3rd</option>
+          <option value="4">4th</option>
+          <option value="5">5th</option>
+          <option value="6">6th</option>
+          <option value="7">7th</option>
+          <option value="8">8th</option>
+          <option value="9">9th</option>
+          <option value="10">10th</option>
+          <option value="11">11th</option>
+          <option value="12">12th</option>
+        </select>
+      </div>
+    </section>
+  `;
+}
+
+export function successTemplate(info) {
+  return `
+    <h2>Registration Successful!</h2>
+    <p>Thank you <strong>${info.name}</strong> for registering.</p>
+    <p>You have registered <strong>${info.count}</strong> participant(s) and owe <strong>$${info.total}</strong> in fees.</p>
+  `;
+}
